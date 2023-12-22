9f9af2381602de27983fe5567ecfe41abecc6f74
Abhimanyu sahani <sahanimannu844@gmail.com>
2023-12-22 10:04:15 +0530

    changes in the animation"

--- script.js
+++ script.js
@@ -25,11 +25,11 @@ gsap.from(".dulha",{
         // markers:true,
         start: "top 90%",
         end: "bottum 50%",
-        scrub:1, 
+        scrub:5, 
     },
     scale:0,
     opacity:0,
-delay:1,
+delay:2,
 duration:1,
     rotate:0,
     x:-300,
@@ -45,7 +45,7 @@ gsap.from(".dulhan",{
         // markers:true,
         start: "top 90%",
         end: "bottum 50%",
-        scrub:1, 
+        scrub:5, 
         
     },
     scale:0,
@@ -55,6 +55,46 @@ duration:1,
     rotate:0,
     x:300
 // duration:2,
+})
+
+
+
+gsap.from(".heart", {
+    scrollTrigger:{
+        trigger:".heart",
+        scroller:"body",
+scrub:5,
+start:"top 220%",
+end: "bottom 50%"
+},
+
+scale:0,
+opacity:0,
+duration:1,
+
+rotate:260,
+
+y:100,
+
+})
+
+
+
+gsap.from(".card", {
+    scrollTrigger:{
+trigger:".card",
+scroller:"body",
+scrub:4,
+markers:true,
+start:"top 70%",
+end: "bottom 30%"
+    },
+
+    duration:1,
+    rotateY:90,
+opacity:0,
+stagger:true
+
 })
 
 
