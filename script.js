5f6fa2efd646f7d05f91769e1274b96cf41beb83
Abhimanyu sahani <sahanimannu844@gmail.com>
2023-12-22 09:48:09 +0530

    added animation

--- script.js
+++ script.js
@@ -14,21 +14,46 @@ adioElemnt.pause()
 }
  
 
-const tl = gsap.to(".page2",{
+
+
+gsap.from(".dulha",{
 
     scrollTrigger:{
         trigger:".dulhan img",
         scroller:"body",
        
         // markers:true,
-        start: "top 75%",
-        end: "bottum 20%",
+        start: "top 90%",
+        end: "bottum 50%",
         scrub:1, 
     },
-    scale:1,
-    opacity:1,
+    scale:0,
+    opacity:0,
 delay:1,
+duration:1,
     rotate:0,
+    x:-300,
+// duration:2,
+})
+
+gsap.from(".dulhan",{
+
+    scrollTrigger:{
+        trigger:".dulhan img",
+        scroller:"body",
+       
+        // markers:true,
+        start: "top 90%",
+        end: "bottum 50%",
+        scrub:1, 
+        
+    },
+    scale:0,
+    opacity:0,
+delay:.5,
+duration:1,
+    rotate:0,
+    x:300
 // duration:2,
 })
 
